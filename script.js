46ba08e5cb843670f2b89e744879e3f288999ef5
aidanakmatova13 <akmatova.don@gmail.com>
2021-10-18 22:08:41 +0300

    mvc

--- script.js
+++ script.js
@@ -1,88 +1,9 @@
 const express = require('express')
-const fs = require('fs')
-const {nanoid} = require('nanoid')
-
+const taskRouter = require('./routes/tasksRoute')
 
 const server = express()
-server.use(express.json())
-
-const readData = (fileName) => {
-    try {
-        return JSON.parse(fs.readFileSync(`./tasks/${fileName}.json`, 'utf8'))
-    } catch (e) {
-        return []
-    }
-}
-
-server.get('/api/tasks/', (req, res) => {
-    const data = readData(req.params.category)
-    const filteredData = data
-        .filter(el => !el._isDeleted)
-        .map(el => {
-            return {
-                id: el.taskId,
-                title: el.title,
-                body: el.body
-            }
-            // delete el._isDeleted
-            // delete el._deletedAt
-            // delete el._createdAt
-            // return el
-        })
-    res.json(filteredData)
-})
-
-server.post('/api/tasks/:category', (req, res) => {
-    // console.log(req.params.category)
-    // console.log(req.body)
-    const newTask = {
-        "taskId": nanoid(2),
-        "title": req.body.title,
-        "status": 'new',
-        "_isDeleted": false,
-        "_createdAt": +new Date(),
-        "_deletedAt": null
-    }
-    const data = readData(req.params.category)
-    const updatedTasks = [...data, newTask]
-    fs.writeFileSync(`./tasks/${req.params.category}.json`, JSON.stringify(updatedTasks, null, 2))
-    res.json(newTask)
-})
-
-server.delete('/api/tasks/:category/:id', (req, res) => {
-    const data = readData(req.params.category)
-    const updatedTasks = data.map(el => el.taskId === req.params.id ? {
-        ...el,
-        _isDeleted: true,
-        _deletedAt: +new Date()
-    } : el)
-    fs.writeFileSync(`./tasks/${req.params.category}.json`, JSON.stringify(updatedTasks, null, 2))
-    res.json(updatedTasks)
-})
-
-server.patch('/api/tasks/:category/:id', (req, res) => {
-    const statuses = ['new', 'in progress', 'done', 'blocked']
-    if (statuses.includes(req.body.status)) {
-        const data = readData(req.params.category)
-        const updatedTasks = data.map(el => el.taskId === req.params.id ? {...el, status: req.body.status} : el)
-        fs.writeFileSync(`./tasks/${req.params.category}.json`, JSON.stringify(updatedTasks, null, 2))
-        res.json(updatedTasks)
-    } else {
-        res.status(501).json({'status': "error", 'message': "incorrect status"})
-    }
-})
-
-server.get('/api/tasks/:category/:timespan', (req, res) => {
-    const data = readData(req.params.category)
-    const duration = {
-        "day": 1000 * 60 * 60 * 24,
-        "week": 1000 * 60 * 60 * 24 * 7,
-        "month": 1000 * 60 * 60 * 24 * 7 * 30,
-        "year": 1000 * 60 * 60 * 24 * 365
-    }
-    const filteredData = data.filter(el => +new Date() - el._createdAt < duration[req.params.timespan])
-    res.json(filteredData)
-})
+server.use(express.json()) //middleware
+server.use("./api/tasks", taskRouter) //middleware
 
 
 // let data = [
